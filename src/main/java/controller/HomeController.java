985e4434877ce6ba3181b185e7ac6f08e1390978
quybui2006 <quybui0611@gmail.com>
2026-09-26 22:46:45 +0700

    refactor(base code): base package

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,4 +1,4 @@
-package org.example.swp391group5.controller;
+package controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
